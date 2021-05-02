d4432d831567d41f4ac989e48b34ba8c14ac58d6
Instrate <Instrate@users.noreply.github.com>
2021-05-02 22:04:48 +0300

    -test branch start

---
+++ Project/Order_System/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	string text = "It's me: Mmmario...!";
+	cout << endl << text;
+	system("pause");
+	return !cout.bad();
+}
